b3c46396fb334a0ea21e835af82e0c4907d12747
Alan Almeida <alandavidalmeida@gmail.com>
2023-09-27 18:59:28 -0300

    creating paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
-export default function App() {
-  return (
-    <h1 className="text-3x1 font-bold underline">
-      Hello world!
-    </h1>
-  )
+const App = () => {
+  <main className="relative">
+      Nav
+    <section className="x1:padding-1 wide:padding-r padding-b">
+      Hero
+    </section>
+    <section className="padding">
+      PopularProducts
+    </section>
+    <section className="padding">
+      SuperQuality
+    </section>
+    <section className="padding-x py-10">
+      Services
+    </section>
+    <section className="padding">
+      SpecialOffers
+    </section>
+    <section className="padding">
+      SpecialOffers
+    </section>
+    <section className="padding">
+      SpecialOffers
+    </section>
+    <section className="padding">
+      SpecialOffers
+    </section>
+    
+  </main>
 }
+
+export default App;
