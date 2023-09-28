8ac9858e5b3f0345463966ecb8018f9685b15165
Alan Almeida <alandavidalmeida@gmail.com>
2023-09-28 16:30:09 -0300

    other commit

--- src/App.jsx
+++ src/App.jsx
@@ -16,14 +16,14 @@ const App = () => {
     <section className="padding">
       SpecialOffers
     </section>
-    <section className="padding">
-      SpecialOffers
+    <section className="bg-pale-blue padding">
+      CustomerReviews
     </section>
-    <section className="padding">
-      SpecialOffers
+    <section className="padding-x sm:py-32 py-16 w-full">
+      Subscribe
     </section>
-    <section className="padding">
-      SpecialOffers
+    <section className="bg-black padding-x padding-t pb-8">
+      Footer
     </section>
     
   </main>
